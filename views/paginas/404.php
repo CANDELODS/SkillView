1e34bce4a2b79a3d47df0361601248dc9700e2d9
Juan Sebastian Candelo Balanta <jcandelobalanta@gmail.com>
2025-11-29 13:27:45 -0500

    Se estiliza la página /404 (notFound)

--- views/paginas/404.php
+++ views/paginas/404.php
@@ -1,3 +1,5 @@
-<h1><?php echo $titulo; ?></h1>
-<p>La página que buscas no se ha encontrado, Quieres volver al login?</p>
-<a href="/">Ir a login</a>
+<div class="notFound">
+    <h1 class="notFound__tittle"><?php echo $titulo; ?></h1>
+    <p class="notFound__p">La página que buscas no se ha encontrado, Quieres volver al login?</p>
+    <a href="/" class="notFound__btn">Ir a login</a>
+</div>
